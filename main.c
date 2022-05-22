0a7752c16c4b11cc93bf53a080221b5ee7c1204a
VismutFO <92463006+VismutFO@users.noreply.github.com>
2022-05-22 11:11:28 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef float afunc(float);
+
+float f1(float x);
+float f2(float x);
+float f3(float x);
+
+float root(afunc *f, afunc *g, float a, float b, float eps1)
+{
+
+}
+
+float integral(afunc *f, float a, float b, float eps1)
+{
+
+}
+
+int main()
+{
+	
+	return 0;
+}
