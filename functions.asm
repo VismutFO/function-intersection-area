c51b073e56389ef62fe4d1ae5a9146c97320b78f
VismutFO <92463006+VismutFO@users.noreply.github.com>
2022-05-24 11:27:37 +0300

    Update functions.asm

--- functions.asm
+++ functions.asm
@@ -6,11 +6,23 @@ section .rodata
     float_3 dd 3.0
     float_4 dd 5.0
     float_5 dd 9.5
+    float_6 dd -4.5
+    float_7 dd -50.0
+    float_8 dd -1.5
+    float_9 dd -5.0
 section .bss
     res1 resd 1
 
 section .text
-_f1:
+GLOBAL f1
+GLOBAL f2
+GLOBAL f3
+
+GLOBAL f1d
+GLOBAL f2d
+GLOBAL f3d
+
+f1:
     push ebp ; Стандартный пролог для соглашения cdecl
     mov ebp, esp
     xor eax, eax ; Предварительно обнуляем возвращаемое значение
@@ -28,7 +40,7 @@ _f1:
     fmulp
     pop ebp
     ret
-_f2:
+f2:
     push ebp ; Стандартный пролог для соглашения cdecl
     mov ebp, esp
     xor eax, eax ; Предварительно обнуляем возвращаемое значение
@@ -40,13 +52,52 @@ _f2:
     fsubp
     pop ebp
     ret
-_f3:
+f3:
     push ebp ; Стандартный пролог для соглашения cdecl
     mov ebp, esp
     xor eax, eax ; Предварительно обнуляем возвращаемое значение
     finit
     fld dword[float_4]
     fld dword[ebp+8] ; Помещаем на стек первый параметр a
+    fdivp
+    pop ebp
+    ret
+f1d:
+    push ebp ; Стандартный пролог для соглашения cdecl
+    mov ebp, esp
+    xor eax, eax ; Предварительно обнуляем возвращаемое значение
+    finit
+    fld dword[float_8]
+    
+    fld dword[ebp+8] ; Помещаем на стек первый параметр a
+    fld1
+    faddp
+    
+    fld dword[ebp+8] ; Помещаем на стек первый параметр a
+    fld1
+    faddp
+    
+    fmulp
+    fdivp
+    pop ebp
+    ret
+f2d:
+    push ebp ; Стандартный пролог для соглашения cdecl
+    mov ebp, esp
+    xor eax, eax ; Предварительно обнуляем возвращаемое значение
+    finit
+    fld dword[float_2]
+    pop ebp
+    ret
+f3d:
+    push ebp ; Стандартный пролог для соглашения cdecl
+    mov ebp, esp
+    xor eax, eax ; Предварительно обнуляем возвращаемое значение
+    finit
+    fld dword[float_9]
+    fld dword[ebp+8] ; Помещаем на стек первый параметр a
+    fld dword[ebp+8] ; Помещаем на стек первый параметр a
+    fmulp
     fdivp
     pop ebp
     ret
